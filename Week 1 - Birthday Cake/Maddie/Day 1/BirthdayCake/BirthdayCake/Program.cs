dc611fdbaace457ac9986880e3470b953d27902f
Maddie <maddie.haley.369@gmail.com>
2018-05-29 15:25:31 -0500

    Day 1 Solution

---
+++ Week 1 - Birthday Cake/Maddie/Day 1/BirthdayCake/BirthdayCake/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayCake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("You can blow out " + HowManyCandels() + " candels");
+            Console.ReadLine();
+        }
+
+        static int HowManyCandels()
+        {
+            Console.WriteLine("How old is the birthday person?");
+            var age = Int32.Parse(Console.ReadLine());
+            var intCandels = generateRandCandels(age);
+            int mx = intCandels.Max();
+            return intCandels.Where(x => x == mx).Count();
+        }
+
+        static List<int> generateRandCandels(int age)
+        {
+            List<int> candels = new List<int>();
+            Random rnd = new Random();
+            for (int i = 0; i < age; i++) {
+                candels.Add(rnd.Next(1, age));
+            }
+            Console.WriteLine(string.Join(",", candels));
+            return candels;
+        }
+    }
+}
