fb06c6888d18ce989ac49d47b472ad67596484e1
Ryan Peterson <rtp4jc@gmail.com>
2018-05-31 08:21:29 -0500

    Add day 3 solution with no user created variables

---
+++ Week 1 - Birthday Cake/Ryan/Day 3/Day 3/Generator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day_3
+{
+    internal static class Generator
+    {
+        public static List<int> generator()
+        {
+            Console.Write("Enter child age: ");
+            int age = Int32.Parse(Console.ReadLine());
+            List<int> candleHeights = new List<int>();
+
+            Random rand = new Random();
+            for (int candle = 0; candle < age; candle++)
+            {
+                candleHeights.Add(rand.Next(1, age));
+            }
+
+            Console.WriteLine("Candle heights: "+ String.Join(", ", candleHeights));
+
+            return candleHeights;
+        }
+    }
+
+}
