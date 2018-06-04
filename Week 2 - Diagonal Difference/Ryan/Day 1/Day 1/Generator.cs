0e4123f59a2fd599b53367343ecef6e105d6ce53
Ryan Peterson <rtp4jc@gmail.com>
2018-06-04 09:47:33 -0500

    Completed Day 1 of Week 2

---
+++ Week 2 - Diagonal Difference/Ryan/Day 1/Day 1/Generator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day_1
+{
+    internal static class Generator
+    {
+        public static List<List<int>> generator()
+        {
+            Random rand = new Random();
+            Console.WriteLine("Enter the table size: ");
+            int n = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("This is the table:");
+
+            List<List<int>> grid = new List<List<int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                List<int> line = new List<int>();
+                for (int j = 0; j < n; j++)
+                {
+                    line.Add(rand.Next(-100, 100));
+                }
+                Console.WriteLine(String.Join(" ", line));
+                grid.Add(line);
+            }
+
+            return grid;
+        }
+    }
+
+}
---
+++ Week 2 - Diagonal Difference/Ryan/Day 1/Day 1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day_1
+{
+    class Program
+    {
+        public static int DiagonalDifference(List<List<int>> grid)
+        {
+            int i1 = 0;
+            int j1 = 0;
+            int i2 = grid.Count-1;
+            int j2 = 0;
+
+            int sum1 = 0;
+            int sum2 = 0;
+
+            for (int k = 0; k < grid.Count; k++)
+            {
+                sum1 += grid[i1][j1];
+                sum2 += grid[i2][j2];
+                i1++;
+                j1++;
+                i2--;
+                j2++;
+            }
+
+            return Math.Abs(sum1-sum2);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The maximum sum is " + DiagonalDifference(Generator.generator()));
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
