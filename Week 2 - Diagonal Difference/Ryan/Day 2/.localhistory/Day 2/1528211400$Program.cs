647c9c7fc409290732f439000d533d13cd140ea9
Ryan Peterson <rtp4jc@gmail.com>
2018-06-05 09:13:39 -0500

    Submitted Day 2 without nested loops and using foreach

---
+++ Week 2 - Diagonal Difference/Ryan/Day 2/.localhistory/Day 2/1528211400$Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day_2
+{
+    class Program
+    {
+        public static int DiagonalDifference(List<List<int>> grid)
+        {
+            int distanceFromEdge = 0;
+            int diff = 0;
+            foreach (List<int> row in grid)
+            {
+                diff += row[distanceFromEdge] - row[row.Count - distanceFromEdge];
+                distanceFromEdge++;
+            }
+
+            return Math.Abs(diff);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The sum is " + );
+        }
+    }
+}
