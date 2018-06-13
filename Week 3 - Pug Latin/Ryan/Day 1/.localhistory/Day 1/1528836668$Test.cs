7954867b0bcac05de6cf2bdad07905a7bb0c17f5
Ryan Peterson <rtp4jc@gmail.com>
2018-06-13 11:45:24 -0500

    Move to renamed file

---
+++ Week 3 - Pug Latin/Ryan/Day 1/.localhistory/Day 1/1528836668$Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Day_1
+{
+    [TestFixture]
+    class Test
+    {
+        public static readonly Dictionary<string, string> PigLatinTranslations = new Dictionary<string, string>
+        {
+            {"Welcome", "Ellcomeway"},
+            { "How are you", "Owhay arehay ouyay" },
+            { "pig", "igpay"},
+            { "latin", "atinlay"},
+            { "banana", "ananabay"},
+            {"cheers", "eerschay"},
+            { "shesh", "eshshay"},
+            { "smile", "ilesmay" },
+            {"every", "eryevay"},
+            { "omelet", "eletomay"},
+            { "another", "otheranay" }
+        };
+
+        [Test]
+        public void TestTranslation()
+        {
+            Assert.AreEqual(Program.PigLatinTranslator("hello world"), "ellohay orldway");
+            Assert.AreEqual(Program.PigLatinTranslator(""));
+        }
+    }
+}
