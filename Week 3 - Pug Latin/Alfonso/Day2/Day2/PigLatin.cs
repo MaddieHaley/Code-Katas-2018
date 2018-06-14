77ef08ce0cddfdcb40f4a521c87b86f3ea276e36
alfonsoV26 <40172967+alfonsoV26@users.noreply.github.com>
2018-06-14 11:01:14 -0500

    This is my day 2 and day 3, for some reason it did not work when I uploaded on their respective days and I realized while I was checking

---
+++ Week 3 - Pug Latin/Alfonso/Day2/Day2/PigLatin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Day2
+{
+    public class PigLatin
+    {
+        [Fact]
+        public void TranslatesToPigLatin()
+        {
+            Assert.Equal("ymay", Program.Translate("my"));
+            Assert.Equal("awberrystray", Program.Translate("strawberry"));
+            Assert.Equal("offeecay", Program.Translate("coffee"));
+            Assert.Equal("urrayhay", Program.Translate("hurray"));
+            Assert.Equal("adamyay", Program.Translate("adam"));
+        }
+    }
+}
---
+++ Week 3 - Pug Latin/Alfonso/Day2/Day2/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word to be translated into Pig Latin\n");
+            Console.WriteLine(Translate(Console.ReadLine()));
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+        public static String Translate(string resp)
+        {
+            resp.ToLower();
+            
+            
+            char[] respArray = resp.ToCharArray();
+            string ay = "";
+            if (resp.Contains(' '))
+                
+            {
+                string[] words = resp.Split(' ');
+                foreach (string item in words)
+                {
+                     ay += PigLatin(item) + " ";
+                }
+                
+            }
+            else
+            {
+                ay = PigLatin(resp);
+            }
+            return ay;
+        }
+        static type Type(char letter)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            if(letter == 'y')
+            {
+                return type.y;
+            }
+            else if (vowels.Contains(letter)) {
+                return type.vowel;
+            }
+            else
+            {
+                return type.consonant;
+            }
+        }
+         enum type
+        {
+            consonant,
+            vowel,
+            y
+        } static string PigLatin(string Resp)
+        {
+            string newWord = "";
+            string afterVowel = "";
+            char[] respArray = Resp.ToCharArray();
+            int index = 1;
+            if (Type(respArray[0]) == type.vowel)
+            {
+                newWord = Resp + "yay";
+            }
+            else if (Type(respArray[0]) == type.consonant || Type(respArray[0]) == type.y)
+            {
+                newWord = respArray[0] + newWord;
+                while (index < Resp.Length && Type(respArray[index]) == type.consonant)
+                {
+                    newWord = newWord + respArray[index];
+                    index++;
+                }
+                if (index == (Resp.Length))
+                {
+                    afterVowel = Resp.Substring(index - 1);
+                }
+                else
+                {
+                    afterVowel = Resp.Substring(index);
+                }
+                newWord = afterVowel + newWord;
+                newWord += "ay";
+            }
+            return newWord;
+        }
+    }
+}
