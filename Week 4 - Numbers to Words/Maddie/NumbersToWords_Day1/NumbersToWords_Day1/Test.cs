4fbe75d888e6ff0fb2fc8e0b76c84cd9f034a957
Maddie <maddie.haley.369@gmail.com>
2018-06-19 10:09:10 -0500

    Day 2 Solution

--- Week 4 - Numbers to Words/Maddie/NumbersToWords_Day1/NumbersToWords_Day1/Test.cs
+++ Week 4 - Numbers to Words/Maddie/NumbersToWords_Day1/NumbersToWords_Day1/Test.cs
@@ -17,5 +17,14 @@ namespace NumbersToWords_Day1
             Assert.Equal("forty-six million, eight hundred twenty-three thousand, nine hundred seventy-five", Program.numsToWords("46823975"));
             Assert.Equal("five hundred forty-six decillion, four hundred fifty-six nonillion, five hundred fifteen octillion, six hundred forty-five septillion, six hundred forty-five sextillion, six hundred forty-five quintillion, six hundred forty-four quadrillion, six hundred forty-six trillion, eight hundred twenty-three billion, nine hundred seventy-five million, two hundred thirty-two thousand, three hundred twenty-three", Program.numsToWords("546456515645645645644646823975232323"));
         }
+
+        [Fact]
+        public void shouldTranslateWordsToNums()
+        {
+            Assert.Equal("0", Program.wordsToNums("zero"));
+            Assert.Equal("100", Program.wordsToNums("one hundred"));
+            Assert.Equal("46823975", Program.wordsToNums("forty-six million, eight hundred twenty-three thousand, nine hundred seventy-five"));
+            Assert.Equal("546456515645645645644646823975232323", Program.wordsToNums("five hundred forty-six decillion, four hundred fifty-six nonillion, five hundred fifteen octillion, six hundred forty-five septillion, six hundred forty-five sextillion, six hundred forty-five quintillion, six hundred forty-four quadrillion, six hundred forty-six trillion, eight hundred twenty-three billion, nine hundred seventy-five million, two hundred thirty-two thousand, three hundred twenty-three"));
+        }
     }
 }
