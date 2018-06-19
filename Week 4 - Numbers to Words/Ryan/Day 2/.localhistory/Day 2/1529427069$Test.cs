857362d1e6e6ae3c9af00cfaed7af75e75905c76
Ryan Peterson <rtp4jc@gmail.com>
2018-06-19 14:40:00 -0500

    Finished day 1 and day 2 with double conversion and tests

---
+++ Week 4 - Numbers to Words/Ryan/Day 2/.localhistory/Day 2/1529427069$Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Day_2
+{
+    public class Test
+    {
+        [Fact]
+        public void NumbersToWordsTest()
+        {
+            Assert.Equal("twelve thousand three hundred forty five dollars and zero cents", Program.NumbersToWords("12345"));
+            Assert.Equal("zero", Program.NumbersToWords(0));
+            Assert.Equal("one hundred twenty two trillion four hundred four thousand fifty five dollars and thirty four cents", Program.NumbersToWords("122000000404055.34"));
+        }
+    }
+}
---
+++ Week 4 - Numbers to Words/Ryan/Day 2/Day 2/Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Day_2
+{
+    public class Test
+    {
+        [Fact]
+        public void NumbersToWordsTest()
+        {
+            Assert.Equal("twelve thousand three hundred forty five", Program.NumbersToWords("12345"));
+            Assert.Equal("zero", Program.NumbersToWords("0"));
+            Assert.Equal("one hundred twenty two trillion four hundred four thousand fifty five", Program.NumbersToWords("122000000404055"));
+        }
+
+        [Fact]
+        public void WordsToNumbersTest()
+        {
+            Assert.Equal(12345, Program.WordsToNumbers("twelve thousand three hundred forty five dollars and zero cents"));
+            Assert.Equal(0, Program.WordsToNumbers("zero"));
+            Assert.Equal(122000000404055, Program.WordsToNumbers("one hundred twenty two trillion four hundred four thousand fifty five"));
+        }
+    }
+}
