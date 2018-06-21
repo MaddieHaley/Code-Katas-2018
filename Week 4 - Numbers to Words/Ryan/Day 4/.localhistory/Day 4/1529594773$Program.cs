fbb0cdeac3dc0960a0e662c1d732becf0d6a5596
Ryan Peterson <rtp4jc@gmail.com>
2018-06-21 11:57:43 -0500

    Finished Day 4

---
+++ Week 4 - Numbers to Words/Ryan/Day 4/.localhistory/Day 4/1529594773$Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xunit;
+
+namespace Day_4
+{
+    class Program
+    {
+        public static string NumbersToWords(long number)
+        {
+            string words = String.Empty;
+            return words;
+        }
+
+        public static long WordsToNumbers(string phrase)
+        {
+            long number = 0;
+            return number;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            var phrase = NumbersToWords(long.Parse(Console.ReadLine()));
+            Console.WriteLine("Number in words: " + phrase);
+            Console.WriteLine("Was: " + WordsToNumbers(phrase));
+            Console.ReadKey();
+        }
+    }
+}
