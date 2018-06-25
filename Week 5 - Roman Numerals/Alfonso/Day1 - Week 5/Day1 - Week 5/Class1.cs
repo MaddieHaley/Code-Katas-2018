a69eadc17e35b5fae21024be3ea0ef86cfc92dd2
alfonsoV26 <40172967+alfonsoV26@users.noreply.github.com>
2018-06-25 09:23:15 -0500

    Test Driven with method to get numeral

---
+++ Week 5 - Roman Numerals/Alfonso/Day1 - Week 5/Day1 - Week 5/Class1.cs
@@ -0,0 +1,21 @@
+﻿using Day1___Week_5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Day1
+{
+    public class PigLatin
+    {
+        [Fact]
+        public void TranslatesToPigLatin()
+        {
+            Assert.Equal("MMMMCCCLII", Program.Translate(4352));
+            Assert.Equal("MMMMMMMMDCCXLII", Program.Translate(8742));
+            Assert.Equal("MMDXCV", Program.Translate(2595));
+        }
+    }
+}
