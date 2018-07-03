12af8d76fc5bc8bdc099bb5355a5beeb19647e08
alfonsoV26 <40172967+alfonsoV26@users.noreply.github.com>
2018-07-03 14:06:26 -0500

    test driven day 2

---
+++ Week 6 - Harry Potter/Alfonso/HarryPotterDay2/HarryPotterDay2/Class1.cs
@@ -0,0 +1,16 @@
+﻿using HarryPotterDay2;
+using Xunit;
+namespace HarryPotterDay2
+{
+    public class Class1
+    {
+        [Fact]
+        public void getsPriceofHarryPotter()
+        {
+            Assert.Equal(30, Program.GetPrice("book 1 book 2 book 3 book 4 book 5"));
+            Assert.Equal(15.2, Program.GetPrice("book 2 book 3"));
+            Assert.Equal(21.6, Program.GetPrice("book 3 book 4 book 5"));
+
+        }
+    }
+}
---
+++ Week 6 - Harry Potter/Alfonso/HarryPotterDay2/HarryPotterDay2/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace HarryPotterDay2
+{
+    internal class Program
+    {
+        public static string numbers = "123456789";
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the books for the Harry Potter Series that you want!");
+            Console.WriteLine(GetPrice(Console.ReadLine().ToLower()));
+            Console.ReadLine();
+        }
+
+        public static double GetPrice(string bookNumbersWanted)
+        {
+            string[] bookNumbersWantedArray = bookNumbersWanted.Split(' ');
+            int[] count = {0, 0, 0, 0, 0};
+            double price = 0;
+            int total = 0;
+            foreach (string letter in bookNumbersWantedArray)
+            {
+                if (numbers.Contains(letter))
+                {
+                    switch (int.Parse(letter))
+                    {
+                        case 1: count[0]++;
+                            break;
+                        case 2: count[1]++;
+                            break;
+                        case 3:
+                            count[2]++;
+                            break;
+                        case 4:
+                            count[3]++;
+                            break;
+                        case 5:
+                            count[4]++;
+                            break;
+                    }
+                }
+            }
+
+            for (int i = 0; i < count.Length; i++)
+            {
+                if (count[i] > 0)
+                {
+                    total++;
+                    count[i] -= 1;
+                }
+            }
+
+            switch (total)
+            {
+                case 1:
+                    price = 8 * total;
+                    break;
+                case 2:
+                    price = 8 * total;
+                    price *= .95;
+                    break;
+                case 3:
+                    price = 8 * total;
+                    price *= .90;
+                    break;
+                case 4:
+                    price = 8 * total;
+                    price *= .80;
+                    break;
+                case 5:
+                    price = 8 * total;
+                    price *= .75;
+                    break;
+            }
+
+            for (int i = 0; i < count.Length; i++)
+            {
+                if (count[i] > 0)
+                {
+                    while (count[1] > 0)
+                    {
+                        price += 8;
+                        count[i] -= 1;
+                    }
+                }
+            }
+
+            return price;
+        }
+    }
+}
